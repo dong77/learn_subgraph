d0d2ada4e929996144b948b4853311bea062ed82
wangdong <wangdong@zhongan.io>
2021-02-11 13:10:29 +0800

    more

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Example Subgraph
 
 An example to help you get started with The Graph. For more information see the docs on https://thegraph.com/docs/.
+
+```
+graph deploy \
+    --debug \
+    --node https://api.thegraph.com/deploy/ \
+    --ipfs https://api.thegraph.com/ipfs/ \ dong77/learn_subgraph
+
+```
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,20 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { TokenRegistered } from '../generated/ExchangeV3/ExchangeV3'
+import { Token } from '../generated/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleTokenRegistered(event: TokenRegistered): void {
+  let token = new Token(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
+  token.address = event.params.token.toHex()
+  token.save()
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+// export function handleUpdatedGravatar(event: UpdatedGravatar): void {
+//   let id = event.params.id.toHex()
+//   let gravatar = Gravatar.load(id)
+//   if (gravatar == null) {
+//     gravatar = new Gravatar(id)
+//   }
+//   gravatar.owner = event.params.owner
+//   gravatar.displayName = event.params.displayName
+//   gravatar.imageUrl = event.params.imageUrl
+//   gravatar.save()
+// }
