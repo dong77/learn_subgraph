340f2efb934dba56a536d0d865288376a0792c9d
wangdong <wangdong@zhongan.io>
2021-02-11 14:10:08 +0800

    more

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,9 +1,38 @@
 import { TokenRegistered } from '../generated/ExchangeV3/ExchangeV3'
+import { ERC20 } from '../generated/ExchangeV3/ERC20'
 import { Token } from '../generated/schema'
+import { BigInt } from '@graphprotocol/graph-ts'
+
+const ZeroAddr = '0x0000000000000000000000000000000000000000'
+
+function i32ToString(value:i32):string {
+  return BigInt.fromI32(value).toString();
+}
 
 export function handleTokenRegistered(event: TokenRegistered): void {
-  let token = new Token(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
-  token.address = event.params.token.toHex()
+  let id = event.params.tokenId
+  if (id === 24) return
+
+  let token = new Token(i32ToString(id))
+
+  if (id === 0) {
+    token.address = ZeroAddr
+    token.symbol = "ETH"
+    token.name = "Ether"
+    token.decimals = 18
+  } else if (id === 7) {
+    token.address = event.params.token.toHex()
+    token.symbol = "MKR"
+    token.name = "Maker"
+    token.decimals =  18
+  } else {
+    token.address = event.params.token.toHex()
+    let erc20 = ERC20.bind(event.params.token)
+    token.symbol = erc20.symbol() || token.address.slice(0, 8)
+    token.name = erc20.name() || token.symbol
+    token.decimals = erc20.decimals() || 18
+  }
+
   token.save()
 }
 
